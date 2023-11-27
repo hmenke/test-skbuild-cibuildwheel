9a1353966a52a9c45ef58b0da89d1d304c180759
Henri Menke <henri@henrimenke.de>
2023-11-27 17:39:07 +0100

    Use GSL as a dependency

--- example/example.c
+++ example/example.c
@@ -1,3 +1,10 @@
 #include "example.h"
 
-float square(float x) { return x * x; }
+#include <gsl/gsl_vector_float.h>
+
+float square(float x) {
+    gsl_vector_float * v = gsl_vector_float_alloc(1);
+    gsl_vector_float_set(v, 0, x);
+    gsl_vector_float_mul(v, v);
+    return gsl_vector_float_get(v, 0);
+}
