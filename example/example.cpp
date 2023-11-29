f3f503c786c10e565d48dbed06a41cb9ac589092
Henri Menke <henri@henrimenke.de>
2023-11-29 10:17:25 +0100

    Fix memory leak

--- example/example.cpp
+++ example/example.cpp
@@ -6,5 +6,7 @@ float square(float x) {
     gsl_vector_float * v = gsl_vector_float_alloc(1);
     gsl_vector_float_set(v, 0, x);
     gsl_vector_float_mul(v, v);
-    return gsl_vector_float_get(v, 0);
+    float y = gsl_vector_float_get(v, 0);
+    gsl_vector_float_free(v);
+    return y;
 }
